f099ec5437c546b362ab743dbbb3ad0fac2932d3
Leo1I3 <gilbertleonardorivasariza@gmail.com>
2023-10-18 07:34:42 -0500

    3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Iniciar Sesión</title>
-        <link rel="stylesheet" href="">
+        @vite(['resources/css/app.css','resources/js/app.js'])
     </head>
     <body>
 
