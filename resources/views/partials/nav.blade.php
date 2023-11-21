1db52a923e037348fe185860bf394b62fb9c2871
Santoyo18 <santoyoalejandro262@gmail.com>
2023-11-21 10:40:55 -0500

    BGFHY

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">ALMECEN SENA</a>
+        <a class="navbar-brand" href="#">ALMACEN SENA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown"
             aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -40,11 +40,11 @@
                         CONTACTENOS
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                        <li><a class="dropdown-item" href="https://twitter.com/?lang=es"><img src="public/Images/icono-twitter-iconos-redes-sociales-estilo-corte-papel_505135-257.avif" alt="Twitter Logo"></a></li>
-                        <li><a class="dropdown-item" href="https://www.instagram.com">instragram</a></li>
-                        <li><a class="dropdown-item" href="https://www.facebook.com">facebook</a></li>
-                        <li><a class="dropdown-item" href="https://www.youtube.com">youtube</a></li>
-                        <li><a class="dropdown-item" href="https://www.tiktok.com/en/">tiktok</a></li>
+                        <li><a class="dropdown-item" href="https://twitter.com/?lang=es"><img width="25" src="/Images/icono-twitter-iconos-redes-sociales-estilo-corte-papel_505135-257.avif" alt="Twitter Logo">  Twitter</a></li>
+                        <li><a class="dropdown-item" href="https://www.instagram.com"><img width="25" src="/Images/icono-instagram-logotipo-insignia-moderna_578229-124.avif" alt=""> instragram</a></li>
+                        <li><a class="dropdown-item" href="https://www.facebook.com"><img width="25" src="/Images/footagecrate-facebook-icon-fun-animation@3X.png" alt=""> facebook</a></li>
+                        <li><a class="dropdown-item" href="https://www.youtube.com"><img width="25" src="/Images/images.png" alt=""> youtube</a></li>
+                        <li><a class="dropdown-item" href="https://www.tiktok.com/en/"><img width="25" src="/Images/tik-tok-para-empresas.jpg" alt=""> tiktok</a></li>
                         <li><a class="dropdown-item"
                                 href="https://oferta.senasofiaplus.edu.co/sofia-oferta/inicio-sofia-plus.html">otros</a>
                         </li>
